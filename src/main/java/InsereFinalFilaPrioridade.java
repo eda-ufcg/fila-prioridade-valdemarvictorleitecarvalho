b6b8ffcf85ff84b39acb25e7004abadd8a878e71
Valdemar Carvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2024-09-13 11:11:05 -0300

    Parcial

--- src/main/java/InsereFinalFilaPrioridade.java
+++ src/main/java/InsereFinalFilaPrioridade.java
@@ -9,12 +9,30 @@ public class InsereFinalFilaPrioridade implements FilaPrioridade {
 	}
 	
 	// criar um Pair e adicionar no fim da fila
-	public void add(String elemento, int prioridade) {}
+	public void add(String elemento, int prioridade) {
+		Pair newPair = new Pair(elemento, prioridade);
+
+		if (this.fila.isEmpty()) {
+			this.fila.add(newPair);
+			return;
+		}
+
+		for (int i = this.fila.size() - 1; i >= 0; i--) {
+			if (this.fila.get(i).getPrioridade() <= newPair.getPrioridade()) {
+				this.fila.add(i, newPair);
+				break;
+			}
+		}
+	}
 
 
 	// buscar pelo elemento de maior prioridade na fila.
 	public String removeNext() {
-		return "";
+		Pair pair = this.fila.get(this.fila.size() - 1); 
+		
+		this.fila.remove(this.fila.size() - 1);
+
+		return pair.getElemento();
 	}
 
 }
--- src/main/java/InsereOrdenadoFilaPrioridade.java
+++ src/main/java/InsereOrdenadoFilaPrioridade.java
@@ -9,9 +9,55 @@ public class InsereOrdenadoFilaPrioridade implements FilaPrioridade {
 		this.last = -1;
 		this.head = -1;
 	}
+
+	private boolean isEmpty() {
+		return this.head == -1 && this.tail == -1;
+	}
+
+	private void resize() {
+		Pair[] newFila = new Pair[this.fila.length * 2];
+
+		for (int i = 0; i < newFila.length; i++) {
+			newFila[i] = this.fila[i];
+		}
+		this.fila = newFila;
+	}
+
+	private void shiftParaDireita(int index) {
+		if (index == this.lista.length - 1) throw new IndexOutBoundsException();
+		
+		for (int i = this.last; i > index; i--) {
+			this.lista[i] = this.lista[i - 1];
+		}
+	}
+
+	private void shiftParaEsquerda(int index) {
+		for (int i = index; i < this.last - 1; i++) {
+			this.fila[i] = this.fila[i + 1];
+		}
+	}
 	
 	// criar um Pair e inserir de forma ordenada decrescente no array.
-	public void add(String elemento, int prioridade) {}
+	public void add(String elemento, int prioridade) {
+		if (this.fila.length == this.tail) resize();
+
+		Pair newPair = new Pair(elemento, prioridade);
+
+		if (isEmpty()) {
+			this.head++;
+			this.last++;
+			this.fila[head] = newPair;
+		}
+		else {
+			for (int i = this.head; i <= this.tail; i++) {
+				if (this.fila[i].getPrioridade() >= newPair.getPrioridade()) {
+					shiftParaDireita();
+					this.fila[i] = newPair;
+					return;
+				}
+			}			
+		}
+	}
 
 
 	// remover e retornar o primeiro elemento do array, que é o de maior prioridade. lembrar manipular head e tail
